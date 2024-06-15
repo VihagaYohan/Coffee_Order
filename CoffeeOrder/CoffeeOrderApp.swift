55c389934525b91d89eb293f5a31d017a50581cf
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-06-15 11:44:30 +0530

    Initial Commit

---
+++ CoffeeOrder/CoffeeOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeOrderApp.swift
+//  CoffeeOrder
+//
+//  Created by Yohan on 2024-06-15.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
